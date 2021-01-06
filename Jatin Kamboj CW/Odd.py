87573caf4419fc46f0f4f85af9244b8b81ac3238
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-06 23:06:54 +0530

    Add files via upload

---
+++ Jatin Kamboj CW/Odd.py
@@ -0,0 +1,7 @@
+#Programe to check if a number is odd or even
+#By Jatin Kamboj
+x=int(input("Enter your number"))
+if x%2==0:
+    print( "This is an even number")
+else:
+    print( "This is an odd number")
