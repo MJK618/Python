402949e09782ef8a6d8f3646dc233cc56351daca
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-06 23:06:03 +0530

    Add files via upload

---
+++ Jatin Kamboj CW/Math Table.py
@@ -0,0 +1,7 @@
+#Mathematical Table
+#By Jatin Kamboj
+while (1):
+    n = int(input("Enter The Number"))
+    print("Mathematical Table")
+    for i in range (1,11):
+        print (n,"*",i,"=",n * i)
