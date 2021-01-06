47c6873290f0cd5d9c287182b34d9c6fbf7f726e
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-06 23:00:14 +0530

    Add files via upload

---
+++ Jatin Kamboj CW/Leap Year.py
@@ -0,0 +1,16 @@
+#Programe to check if the given year is leap year or not
+#By Jatin Kamboj
+
+y=int(input("Enter The Year"))
+if ((y%4==0)&(y%100!=0))|(y%400==0):
+    print("This is a leap year")
+else:
+    print("This is not a leap year") 
+    
+   
+    
+    
+
+   
+  
+  
