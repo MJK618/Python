7bec9fa91e0d762e435118debf6d3476db660c15
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-06 22:58:04 +0530

    Add files via upload

---
+++ Jatin Kamboj CW/Database.py
@@ -0,0 +1,32 @@
+#By Jatin Kamboj
+
+n=input("Student Name")
+
+M=int(input("Enter your marks in Mathematics"))
+
+P=int(input("Enter your marks in Physics"))
+
+C=int(input("Enter your marks in Chemistry"))
+
+x=(M+P+C)/3
+
+print ("*********************REPORT CARD***********************")
+
+print(("Student name-"),(n))
+
+print(("Your percentage is-"),(x))
+
+if (x>=90):
+    print("\nGrade Obtained A")
+elif (x>=80 and x<90):
+    print("\nGrade Obtained B")
+elif (x>=70 and x<80):
+    print("\nGrade Obtained C")
+elif (x<70 and x>=60):
+    print("\nGrade Obtained D")
+elif (x>60 and x<=50):
+    print("\nGrade Obtained E")
+else:
+    print("\nYou failed")
+    
+    
