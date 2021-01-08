31fb8ebc079f29cb2fe129690569f3dc6116570c
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-08 22:20:12 +0530

    Add files via upload

---
+++ Jatin Kamboj CW/Selection Sort.py
@@ -0,0 +1,21 @@
+#Selction Sort
+#By Jatin Kamboj
+A=[]
+n=int(input("Enter The Number Of Elements To Be Entered"))
+A=[0]*n
+for i in range (0,n):
+    A[i]=int(input("Enter The Values In The Array"))
+print(A)
+
+for j in range(0,n):
+    small=A[j]
+    index=j
+    for i in range (j+1,n):
+        if A[i]<small:
+            small=A[i]
+            index=i
+    A[index]=A[j]
+    A[j]=small
+print("\t\t\t\tSorted Array\t\t\t\t")
+print(A)        
+    
