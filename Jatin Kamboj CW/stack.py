fd9472404ab9597a882d78ba14a72e163cc85f2a
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-08 22:20:48 +0530

    Add files via upload

---
+++ Jatin Kamboj CW/stack.py
@@ -0,0 +1,33 @@
+#Stack
+#By Jatin Kamboj
+class Stack(object):
+    def __init__(self):
+       self.Stack=[]
+    def push(self,value):
+       self.Stack.append(value)
+    def pop(self):
+      return (self.Stack.pop())    
+    def display(self):    
+      return self.Stack
+    def  isempty(self):
+      return len(self.Stack)
+S1=Stack()
+while(1):
+    print("1-Push a Value")
+    print("2-Pop")
+    print("3-Display")
+    ch=int(input("Enter Your Choice"))
+    if ch==1:
+        v=int(input("Enter Value"))
+        S1.push(v)
+    elif ch==2:\
+        print(S1.pop())
+    elif ch==3:
+       print(S1.display())
+    else:
+        S1.isempty()
+
+       
+                   
+
+
