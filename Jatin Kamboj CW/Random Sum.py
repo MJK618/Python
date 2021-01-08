f8ed3aa46ea4d435eb60a027f6ca98f8075a4643
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-08 22:19:48 +0530

    Add files via upload

---
+++ Jatin Kamboj CW/Random Sum.py
@@ -0,0 +1,9 @@
+#Sum of Random Numbers
+#By Jatin Kamboj
+n=int(input("Enter Total Number Of Terms"))
+Sum=0
+for i in range(1,n+1):
+    Num=int(input("Enter Number"))
+    Sum=Sum+Num
+    print(("Sum="),(Sum))
+    
