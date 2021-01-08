7e8f70f010640dd2619b7c5f170a88814aa10b2e
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-08 22:28:10 +0530

    Add files via upload

---
+++ Jatin Kamboj CW/Summation USing While Loop.py
@@ -0,0 +1,10 @@
+#Summation Of n Natural Numbers Using While Loop
+#By Jatin Kamboj
+i=1
+Sum=0
+n=int(input("Enter Number Of Terms"))
+while(i<=n):
+      Sum=Sum+i
+      i=i+1
+      print(Sum)
+     
