66ad627c4f3b78350e63505ad156572e01f295c6
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-11 17:35:36 +0530

    Add files via upload

---
+++ Jatin Kamboj CW/3.6.0/Operators.py
@@ -0,0 +1,28 @@
+#Programe to use all operators
+#By Jatin Kamboj
+print("\t\t\t\tPrograme to use all operators")
+print("Enter two numbers")
+
+x=int(input("Enter First Number"))
+y=int(input("Enter Second Number"))
+
+print("\n Arithematic Opertions")
+
+print("\n x+y=",x+y)
+print("\n x-y",x-y)
+print("\n x*y",x*y)
+
+print("Bitwise Operators")
+
+print("x<<2",x<<2)
+print("y>>2",y>>2)
+if x<=y:
+   print("x is the smaller number")
+else:
+   print("y is the smaller number")
+
+print("\n Logical Operators")
+
+print("\n x&y",x&y)
+print("\n x|y",x|y)
+
