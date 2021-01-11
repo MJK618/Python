db989830e7ee642cc1c68c4afb267269cc600a23
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-11 17:31:45 +0530

    Add files via upload

--- Jatin Kamboj CW/Math Table.py
+++ Jatin Kamboj CW/Math Table.py
@@ -1,7 +1,12 @@
 #Mathematical Table
 #By Jatin Kamboj
-while (1):
-    n = int(input("Enter The Number"))
-    print("Mathematical Table")
-    for i in range (1,11):
-        print (n,"*",i,"=",n * i)
+while(1):
+    print("\t\t\t\tMath Table")
+    print("Press 0 To Exit")
+    n=int(input("Enter The Number Of Which Table Is To Be Generated"))
+    if n==0:
+        exit()
+    else:
+        print("Mathematical Table")
+        for i in range (1,11):
+            print (n,"*",i,"=",n*i)
