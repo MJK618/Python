3b176748752f1bae8d3fb2df9bc2fb02e195294e
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-01-11 17:31:25 +0530

    Add files via upload

---
+++ Jatin Kamboj CW/3.6.0/Loop.py
@@ -0,0 +1,9 @@
+#Summation of first n terms using loop
+#By Jatin Kamboj
+Sum=0
+i=1
+n=int(input("Enter Number Of Terms"))
+for i in range(1,n+1):
+    Sum=Sum+i
+    print(("Sum="),(Sum))
+                                                                                                
